f0ca283f8120e7d7f25f00d44a1d58733f4a7c0b
Andrés Martínez <autummata@gmail.com>
2025-03-12 00:05:12 -0600

    Refactor: Removed /todolist from all routes in backend repository

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/ToDoItemController.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/ToDoItemController.java
@@ -1,4 +1,5 @@
 package com.springboot.MyTodoList.controller;
+
 import com.springboot.MyTodoList.model.ToDoItem;
 import com.springboot.MyTodoList.service.ToDoItemService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,65 +7,65 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-
-import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/todolist")
 public class ToDoItemController {
     @Autowired
     private ToDoItemService toDoItemService;
-    //@CrossOrigin
-    @GetMapping(value = "/todolist")
-    public List<ToDoItem> getAllToDoItems(){
+
+    // @CrossOrigin
+    @GetMapping
+    public List<ToDoItem> getAllToDoItems() {
         return toDoItemService.findAll();
     }
-    //@CrossOrigin
-    @GetMapping(value = "/todolist/{id}")
-    public ResponseEntity<ToDoItem> getToDoItemById(@PathVariable int id){
-        try{
+
+    // @CrossOrigin
+    @GetMapping("/{id}")
+    public ResponseEntity<ToDoItem> getToDoItemById(@PathVariable int id) {
+        try {
             ResponseEntity<ToDoItem> responseEntity = toDoItemService.getItemById(id);
             return new ResponseEntity<ToDoItem>(responseEntity.getBody(), HttpStatus.OK);
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-    //@CrossOrigin
-    @PostMapping(value = "/todolist")
-    public ResponseEntity addToDoItem(@RequestBody ToDoItem todoItem) throws Exception{
+
+    // @CrossOrigin
+    @PostMapping
+    public ResponseEntity addToDoItem(@RequestBody ToDoItem todoItem) throws Exception {
         ToDoItem td = toDoItemService.addToDoItem(todoItem);
         HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set("location",""+td.getID());
-        responseHeaders.set("Access-Control-Expose-Headers","location");
-        //URI location = URI.create(""+td.getID())
+        responseHeaders.set("location", "" + td.getID());
+        responseHeaders.set("Access-Control-Expose-Headers", "location");
+        // URI location = URI.create(""+td.getID())
 
         return ResponseEntity.ok()
                 .headers(responseHeaders).build();
     }
-    //@CrossOrigin
-    @PutMapping(value = "todolist/{id}")
-    public ResponseEntity updateToDoItem(@RequestBody ToDoItem toDoItem, @PathVariable int id){
-        try{
+
+    // @CrossOrigin
+    @PutMapping("/{id}")
+    public ResponseEntity updateToDoItem(@RequestBody ToDoItem toDoItem, @PathVariable int id) {
+        try {
             ToDoItem toDoItem1 = toDoItemService.updateToDoItem(id, toDoItem);
             System.out.println(toDoItem1.toString());
-            return new ResponseEntity<>(toDoItem1,HttpStatus.OK);
-        }catch (Exception e){
+            return new ResponseEntity<>(toDoItem1, HttpStatus.OK);
+        } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
-    //@CrossOrigin
-    @DeleteMapping(value = "todolist/{id}")
-    public ResponseEntity<Boolean> deleteToDoItem(@PathVariable("id") int id){
+
+    // @CrossOrigin
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> deleteToDoItem(@PathVariable("id") int id) {
         Boolean flag = false;
-        try{
+        try {
             flag = toDoItemService.deleteToDoItem(id);
             return new ResponseEntity<>(flag, HttpStatus.OK);
-        }catch (Exception e){
-            return new ResponseEntity<>(flag,HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(flag, HttpStatus.NOT_FOUND);
         }
     }
-
-
-
 }
