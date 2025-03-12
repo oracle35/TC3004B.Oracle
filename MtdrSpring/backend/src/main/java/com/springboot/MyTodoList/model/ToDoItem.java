ea4b2c14ef2b2939716c5302495809bfdcefae1f
Andrés Martínez <autummata@gmail.com>
2025-03-12 00:11:25 -0600

    Update: To-Do Item Model documentation

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/model/ToDoItem.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/model/ToDoItem.java
@@ -1,13 +1,40 @@
+/**
+ * Represents a ToDoItem entity mapped to the TODOITEM table in the database.
+ * This class is used to store and retrieve information about to-do items.
+ * 
+ * Fields:
+ * - ID: The unique identifier for the to-do item.
+ * - description: A brief description of the to-do item.
+ * - creation_ts: The timestamp when the to-do item was created.
+ * - done: A boolean indicating whether the to-do item is completed.
+ * 
+ * Constructors:
+ * - ToDoItem(): Default constructor.
+ * - ToDoItem(int ID, String description, OffsetDateTime creation_ts, boolean done): Parameterized constructor.
+ * 
+ * Methods:
+ * - getID(): Returns the ID of the to-do item.
+ * - setID(int ID): Sets the ID of the to-do item.
+ * - getDescription(): Returns the description of the to-do item.
+ * - setDescription(String description): Sets the description of the to-do item.
+ * - getCreation_ts(): Returns the creation timestamp of the to-do item.
+ * - setCreation_ts(OffsetDateTime creation_ts): Sets the creation timestamp of the to-do item.
+ * - isDone(): Returns whether the to-do item is completed.
+ * - setDone(boolean done): Sets the completion status of the to-do item.
+ * - toString(): Returns a string representation of the to-do item.
+ */
+
 package com.springboot.MyTodoList.model;
 
 
-import javax.persistence.*;
 import java.time.OffsetDateTime;
 
-/*
-    representation of the TODOITEM table that exists already
-    in the autonomous database
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 @Entity
 @Table(name = "TODOITEM")
 public class ToDoItem {
