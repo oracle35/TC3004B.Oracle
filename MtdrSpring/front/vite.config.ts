2cf70eaa2fd3264c37d965f3d8a33acc85565f3d
Andrés Martínez <autummata@gmail.com>
2025-03-22 22:52:35 -0600

    Remove: Vite Eslint Plugin

--- MtdrSpring/front/vite.config.ts
+++ MtdrSpring/front/vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import eslint from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), eslint()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
