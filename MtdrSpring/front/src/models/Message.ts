86d4dd66ce9f25180100b83a103c276b4797a594
Dasaba03 <david.sanchez@live.com.mx>
2025-04-03 17:01:28 -0600

    Interfaces faltantes

---
+++ MtdrSpring/front/src/models/Message.ts
@@ -0,0 +1,12 @@
+/**
+ * Message model
+ * This model represents a message in the system
+ * It contains information about the message such as its ID, content, date/time posted,
+ * and the ID (user or entity) to whom it is addressed (notified).*/
+export interface Message {
+    id_message: number;
+    content: string;
+    postedAt: Date;        
+    notificateTo: number;
+  }
+  
