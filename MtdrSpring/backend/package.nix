d306c020c324001416ac7f83e61210abc98888d2
Victor Quintana <435victorjavier@gmail.com>
2025-04-20 00:52:55 -0600

    frontend nix: switch to maven.buildMavenPackage

--- MtdrSpring/backend/package.nix
+++ MtdrSpring/backend/package.nix
@@ -1,43 +1,25 @@
 {
   lib,
-  stdenv,
-  buildMavenRepositoryFromLockFile,
-  makeWrapper,
   maven,
   jdk11_headless,
   todoapp-frontend,
   ...
-}: let
-  mavenRepository = buildMavenRepositoryFromLockFile {file = ./mvn2nix-lock.json;};
-in
-  stdenv.mkDerivation rec {
+}:
+  maven.buildMavenPackage rec {
     pname = "MyTodoList";
     version = "0.0.1-SNAPSHOT";
     name = "${pname}-${version}";
 
     src = ./.;
 
-    nativeBuildInputs = [jdk11_headless maven makeWrapper];
-
-    patchPhase = ''
-      substituteInPlace ./src/main/resources/application.properties \
-        --replace-fail "@walletPath@" "${src}/wallet"
-    '';
-
-    buildPhase = ''
-      mkdir -p target
+    preBuild = ''
       ln -s ${todoapp-frontend} ./target/frontend
-      mvn package --offline -Dmaven.repo.local=${mavenRepository}
     '';
 
-    passthru = {
-      inherit mavenRepository;
-    };
+    mvnHash = lib.fakeHash;
 
     installPhase = ''
       mkdir -p $out/bin
-
-      ln -s ${mavenRepository} $out/lib
 
       cp target/${name}.jar $out/
       cp -r $src/wallet $out/
