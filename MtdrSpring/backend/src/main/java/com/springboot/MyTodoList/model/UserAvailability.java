b13e32044bf8edb39ea7a70f4137610c29e0cbb0
andresdanielmtz <autummata@gmail.com>
2025-04-23 23:23:14 -0600

    Add: User Availability Entity to Backend

---
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/model/UserAvailability.java
@@ -0,0 +1,47 @@
+package com.springboot.MyTodoList.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="USERAVAILABILITY")
+public class UserAvailability {
+    @Id
+    int ID_USER;
+
+    @Column(name = "AVAILABLEHOURS")
+    Integer AVAILABLE_HOURS;
+
+    public UserAvailability() {
+    }
+
+    public UserAvailability(int ID_USER, Integer AVAILABLE_HOURS) {
+        this.ID_USER = ID_USER;
+        this.AVAILABLE_HOURS = AVAILABLE_HOURS;
+    }
+
+    public int getID_USER() {
+        return ID_USER;
+    }
+
+    public void setID_USER(int ID_USER) {
+        this.ID_USER = ID_USER;
+    }
+
+    public Integer getAVAILABLE_HOURS() {
+        return AVAILABLE_HOURS;
+    }
+
+    public void setAVAILABLE_HOURS(Integer AVAILABLE_HOURS) {
+        this.AVAILABLE_HOURS = AVAILABLE_HOURS;
+    }
+
+    @Override
+    public String toString() {
+        return "UserAvailability" +
+                "ID_USER=" + ID_USER +
+                ", AVAILABLE_HOURS=" + AVAILABLE_HOURS;
+    }
+}
