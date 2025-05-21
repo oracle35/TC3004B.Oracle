7543df806a5c1a6626ec700842120c00b116e290
andresdanielmtz <autummata@gmail.com>
2025-05-20 23:12:34 -0600

    Run: Prettier

--- MtdrSpring/front/src/pages/Pending/Pending.tsx
+++ MtdrSpring/front/src/pages/Pending/Pending.tsx
@@ -4,7 +4,9 @@ const PendingTasks = () => {
   return (
     <div style={{ padding: "1rem" }}>
       <Typography variant="h4">Pendientes</Typography>
-      <Typography variant="body1">Aquí aparecerán las tareas pendientes.</Typography>
+      <Typography variant="body1">
+        Aquí aparecerán las tareas pendientes.
+      </Typography>
     </div>
   );
 };
