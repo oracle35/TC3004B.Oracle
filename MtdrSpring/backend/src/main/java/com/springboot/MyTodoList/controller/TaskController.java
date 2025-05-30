8c42f40bfca557e8359bee45c7b1d96535f0dc21
andresdanielmtz <autummata@gmail.com>
2025-05-30 09:19:36 -0600

    Add: Sprint Length Indicator

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/TaskController.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/TaskController.java
@@ -1,19 +1,29 @@
 package com.springboot.MyTodoList.controller;
 
-import com.springboot.MyTodoList.model.Task;
-import com.springboot.MyTodoList.service.TaskService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.springboot.MyTodoList.model.Task;
+import com.springboot.MyTodoList.service.TaskService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
  * TODO: Implementar Query Params para la busqueda de tareas.
@@ -79,7 +89,7 @@ public class TaskController {
             summary = "Actualizar una tarea existente",
             description = "Actualiza los datos de una tarea específica basado en su ID.")
     public ResponseEntity<Task> updateTask(@Valid @RequestBody Task taskToBeUpdated, @PathVariable int id) {
-        Task task = taskService.updateTask(id, taskToBeUpdated);
+        Optional<Task> task = taskService.updateTask(id, taskToBeUpdated);
         return task.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
