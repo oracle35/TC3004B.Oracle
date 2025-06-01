b0328c3f67c5f74f79352f775d0c93f9125e6dae
andresdanielmtz <autummata@gmail.com>
2025-06-01 15:16:31 -0600

    Add: Set Sprint to Backlog Task

--- MtdrSpring/front/src/components/Backlog/Backlog.tsx
+++ MtdrSpring/front/src/components/Backlog/Backlog.tsx
@@ -2,9 +2,13 @@ import {
   Box,
   Divider,
   Drawer,
+  FormControl,
+  IconButton,
   List,
   ListItem,
   ListItemText,
+  MenuItem,
+  Select,
   Typography,
 } from "@mui/material";
 import { Task } from "../../models/Task";
@@ -12,7 +16,8 @@ import { Sprint } from "../../models/Sprint";
 import { useEffect, useState } from "react";
 import styles from "./Backlog.module.css";
 import { Subtitle } from "../Subtitle";
-
+import { updateTask } from "../../api/task";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 interface BacklogDrawerProps {
   open: boolean;
   onClose: (arg0: boolean) => void;
@@ -33,6 +38,33 @@ const BacklogDrawer = ({
   sprints,
 }: BacklogDrawerProps) => {
   const [backlogTasks, setBacklogTasks] = useState<Task[]>([]);
+  const [selectedSprintForTask, setSelectedSprintForTask] = useState<{
+    [taskId: number]: number;
+  }>({});
+
+  const handleMoveToSprint = (task: Task, sprintId: number) => {
+    if (!sprintId) return;
+
+    // Update the task to move it to the selected sprint
+    // TODO: Change updateTask to just give the attributes that are needed and not the entire task.
+    updateTask(task.id_Task, {
+      ...task,
+      id_Sprint: sprintId,
+    })
+      .then(() => {
+        setBacklogTasks((prevTasks) =>
+          prevTasks.filter((t) => t.id_Task !== task.id_Task),
+        );
+        setSelectedSprintForTask((prev) => {
+          const newState = { ...prev };
+          delete newState[task.id_Task]; // Remove the task from selectedSprintForTask
+          return newState;
+        });
+      })
+      .catch((error) => {
+        console.error("Failed to update task:", error);
+      });
+  };
 
   useEffect(() => {
     // Filter tasks where id_Sprint is exactly -1
@@ -62,7 +94,11 @@ const BacklogDrawer = ({
         {backlogTasks.length > 0 ? (
           <List>
             {backlogTasks.map((task) => (
-              <ListItem key={task.id_Task} disablePadding>
+              <ListItem
+                key={task.id_Task}
+                disablePadding
+                sx={{ alignItems: "flex-start" }}
+              >
                 <ListItemText
                   primary={task.description}
                   secondary={`Created on: ${
@@ -70,6 +106,47 @@ const BacklogDrawer = ({
                       ?.startsAt || "(No date available)"
                   }`}
                 />
+                <FormControl size="small" sx={{ minWidth: 120, ml: 2 }}>
+                  <Select
+                    displayEmpty
+                    value={selectedSprintForTask[task.id_Task] || ""}
+                    onChange={(e) =>
+                      setSelectedSprintForTask((prev) => ({
+                        ...prev,
+                        [task.id_Task]: Number(e.target.value),
+                      }))
+                    }
+                    renderValue={(selected) =>
+                      selected
+                        ? sprints.find((s) => s.id_Sprint === selected)?.name
+                        : "Add to Sprint"
+                    }
+                  >
+                    {sprints
+                      .filter((s) => s.id_Sprint !== -1)
+                      .map((sprint) => (
+                        <MenuItem
+                          key={sprint.id_Sprint}
+                          value={sprint.id_Sprint}
+                        >
+                          {sprint.name}
+                        </MenuItem>
+                      ))}
+                  </Select>
+                </FormControl>
+                <IconButton
+                  color="primary"
+                  sx={{ ml: 1 }}
+                  disabled={!selectedSprintForTask[task.id_Task]}
+                  onClick={() =>
+                    handleMoveToSprint(
+                      task,
+                      selectedSprintForTask[task.id_Task],
+                    )
+                  }
+                >
+                  <ArrowForwardIcon />
+                </IconButton>
               </ListItem>
             ))}
           </List>
